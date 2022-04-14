fe4d171ea73d30881622cb1527155bd1195cf20d
krupa2002 <dharamshikrupa@gmail.com>
2022-04-14 18:02:01 +0530

    Dashboard

---
+++ app/src/main/java/com/example/dashboard/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.dashboard;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
+        NavigationBarView.OnItemSelectedListener;
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+
+    }
+    private NavigationBarView.OnItemSelectedListener navigationItemSelectedListner=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+            Fragment selectedFragment=null;
+            switch (item.getItemId()){
+                case R.id.home:
+                    selectedFragment=new HomeFragment();
+                    break;
+                case R.id.profile:
+                    selectedFragment=new ProfileFragment();
+                    break;
+                case R.id.setting:
+                    selectedFragment=new SettingFragment();
+                    break;
+
+
+
+
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+    };
+};}
